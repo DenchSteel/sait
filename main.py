daea64ca635906ae46a176d3945f2175ca818953
Shamil <89793572+EmptyKaktus@users.noreply.github.com>
2023-05-02 18:16:55 +0300

    тест

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def load_user(user_id):
 
 
 @app.route("/")
-def index():
+def news():
     db_sess = db_session.create_session()
     if current_user.is_authenticated:
         news = db_sess.query(News).filter(
@@ -40,6 +40,11 @@ def index():
         news = db_sess.query(News).filter(News.is_private != True)
     news = db_sess.query(News).filter(News.is_private != True)
     return render_template("index.html", news=news)
+
+
+@app.route("/stock")
+def stock():
+    pass
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -93,18 +98,21 @@ def logout():
 @login_required
 def add_news():
     form = NewsForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        news = News()
-        news.title = form.title.data
-        news.content = form.content.data
-        news.is_private = form.is_private.data
-        current_user.news.append(news)
-        db_sess.merge(current_user)
-        db_sess.commit()
-        return redirect('/')
-    return render_template('news.html', title='Добавление новости',
-                           form=form)
+    if request.method == "GET":
+        if form.validate_on_submit():
+            db_sess = db_session.create_session()
+            news = News()
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            current_user.news.append(news)
+            db_sess.merge(current_user)
+            db_sess.commit()
+            return redirect('/')
+        return render_template('news.html', title='Добавление новости',
+                               form=form)
+    if request.method == "POST":
+        return
 
 
 @app.route('/news/<int:id>', methods=['GET', 'POST'])
