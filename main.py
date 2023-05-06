61346b65125681ec251f9169a7935811a7ebdcad
Shamil <89793572+EmptyKaktus@users.noreply.github.com>
2023-05-06 17:45:38 +0300

    отображение текста "о сайте" отображенеи курса валют

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from data.users import User, LoginForm
 from forms.news import NewsForm
 from forms.user import RegisterForm
 from data import db_session, news_api
+from data.val import Currency
 
 
 app = Flask(__name__)
@@ -45,6 +46,23 @@ def news():
 @app.route("/stock")
 def stock():
     pass
+
+
+@app.route("/currency")
+def currency():
+    USD_ = 'https://www.google.com/search?sxsrf=ALeKk01NWm6viYijAo3HXYOEQUyDEDtFEw%3A1584716087546&source=hp&ei=N9l0XtDXHs716QTcuaXoAg&q=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&oq=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+&gs_l=psy-ab.3.0.35i39i70i258j0i131l4j0j0i131l4.3044.4178..5294...1.0..0.83.544.7......0....1..gws-wiz.......35i39.5QL6Ev1Kfk4'
+    EUR_ = "https://www.google.com/search?q=%D0%B5%D0%B2%D1%80%D0%BE+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&sxsrf=APwXEdde0l1Zlvua1JTMXxovQDoOrb3yyQ%3A1683356678622&ei=BvxVZKXQJYf1rgTuvI_wBg&oq=%D0%B5%D0%B2%D1%80%D0%BE&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMg0IABCKBRCxAxCDARBDMg0IABCKBRCxAxCDARBDMg0IABCKBRCxAxCDARBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDMgcIABCKBRBDOgoIABBHENYEELADOgoIABCKBRCwAxBDOgUIABCABDoKCAAQgAQQFBCHAjoHCAAQgAQQCjoGCAAQFhAeOgkIABCABBAKECo6CAgAEBYQHhAKOgcIIxCKBRAnOgsIABCABBCxAxCDAToICAAQgAQQsQM6DAgjEIoFECcQRhCCAjoQCAAQgAQQFBCHAhCxAxCDAToOCAAQgAQQsQMQgwEQyQM6CAgAEIAEEJIDOgsIABCKBRCxAxCDAToRCC4QgAQQsQMQgwEQxwEQ0QM6CwguEIoFELEDEIMBOgcIIxDqAhAnOg8IABCKBRDqAhC0AhBDGAE6CwguEIMBELEDEIoFSgQIQRgAUN8FWOdPYLdaaANwAXgBgAH2A4gBlhiSAQwwLjExLjIuMS4wLjGYAQCgAQGwARTIAQrAAQHaAQYIARABGAE&sclient=gws-wiz-serp"
+    CNY_ = "https://www.google.com/search?q=%D1%8E%D0%B0%D0%BD%D1%8C+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&sxsrf=APwXEdeXRCPWu48hol3cebaiTBtgJMqWKA%3A1683356745180&ei=SfxVZNXRCoKSwPAPtpiooAo&oq=%D1%8E+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB5KBAhBGABQAFjWHWDlMGgBcAF4AIABswOIAZgHkgEHMC4zLjQtMZgBAKABAcABAQ&sclient=gws-wiz-serp"
+    GBP_ = "https://www.google.com/search?q=%D1%84%D1%83%D0%BD%D1%82+%D1%81%D1%82%D0%B5%D1%80%D0%BB%D0%B8%D0%BD%D0%B3%D0%BE%D0%B2+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&sxsrf=APwXEdcKsJg6HSBV_oUN6QoPqRV0KvwQVw%3A1683358015833&ei=PwFWZPLDMo-yqwHropH4CQ&oq=%D1%84%D1%83%D0%BD%D1%82+%D1%81+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB4yBggAEAcQHjIGCAAQBxAeMgYIABAHEB46CggAEA0QgAQQsQM6BwgAEA0QgAQ6CggAEIoFELEDEEM6BwgAEIoFEEM6BQgAEIAEOg8IABCKBRCxAxBDEEYQggJKBAhBGABQAFiPGmDHKWgAcAF4AIABhAOIAdwIkgEHMC41LjAuMZgBAKABAcABAQ&sclient=gws-wiz-serp"
+    CHF_ = "https://www.google.com/search?q=%D1%88%D0%B2%D0%B5%D0%B9%D1%86%D0%B0%D1%80%D1%81%D0%BA%D0%B8%D0%B9+%D1%84%D1%80%D0%B0%D0%BD%D0%BA+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&sxsrf=APwXEdfiPLT_-vQy5lam1PDvIFiTFtKGzQ%3A1683358517974&ei=NQNWZOaOO-bIrgS_-LvYDg&oq=%D1%89+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgBMgsIABAHEB4Q8QQQCjILCAAQBxAeEPEEEAoyCAgAEAgQBxAeMgoIABAFEAcQHhAKOgoIABBHENYEELADOgoIABCKBRCwAxBDSgQIQRgAULsOWLsOYPEfaAFwAXgAgAGXAYgBlwGSAQMwLjGYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp"
+
+    USD = Currency(USD_).check_currency("Доллар")
+    EUR = Currency(EUR_).check_currency("Евро")
+    CNY = Currency(CNY_).check_currency("Юань")
+    GBP = Currency(GBP_).check_currency("Фунт стерлинга")
+    CHF = Currency(CHF_).check_currency("Швейцарский франк")
+
+    return render_template("o_sait.html", title="Курс валют", usd=USD, eur=EUR, cny=CNY, gbr=GBP, chf=CHF)
 
 
 @app.route("/")
